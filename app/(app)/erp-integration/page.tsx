66e8206e782a34d103074fdf4dce58116571f215
SHWFT <seanshtiv@gmail.com>
2025-08-29 14:41:15 -0500

    chore(ui): add stub pages for nav destinations

--- app/(app)/erp-integration/page.tsx
+++ app/(app)/erp-integration/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold">PO Management</h1>
+      <h1 className="text-3xl font-bold">ERP Integration</h1>
     </div>
   );
 }
